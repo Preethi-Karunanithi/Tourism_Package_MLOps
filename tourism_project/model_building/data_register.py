059e31ddb53c18bed286ef90be5dc702f682377e
Preethi-Karunanithi <preethi.karunanithi@gmail.com>
2026-07-13 03:01:53 +0000

    Remove google colab dependency

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -1,35 +1,41 @@
-
-
-from google.colab import userdata
-from huggingface_hub import HfApi
-
-from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from huggingface_hub import HfApi, create_repo
+from huggingface_hub.utils import RepositoryNotFoundError
 import os
 
 token = os.getenv("HF_TOKEN")
 
+if not token:
+    raise ValueError("HF_TOKEN is missing. Please configure it.")
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
+api = HfApi(token=token)
 
-repo_id = "PreethiKarunanithi/Preethi-first-project-space"    # please create your space and repository
-
+repo_id = "PreethiKarunanithi/Preethi-first-project-space"
 repo_type = "dataset"
 
-# Initialize API client
-api = HfApi(token=os.getenv("HF_TOKEN"))
-
-# Step 1: Check if the space exists
 try:
-    api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Space '{repo_id}' already exists. Using it.")
+    api.repo_info(
+        repo_id=repo_id,
+        repo_type=repo_type
+    )
+    print(f"Dataset repository '{repo_id}' already exists.")
+
 except RepositoryNotFoundError:
-    print(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
-    print(f"Space '{repo_id}' created.")
+    print(f"Dataset repository '{repo_id}' not found. Creating...")
+    
+    create_repo(
+        repo_id=repo_id,
+        repo_type=repo_type,
+        private=False,
+        token=token
+    )
+
+    print(f"Dataset repository '{repo_id}' created.")
+
 
 api.upload_folder(
     folder_path="tourism_project/data",
     repo_id=repo_id,
-    repo_type=repo_type,
+    repo_type=repo_type
 )
+
+print("Dataset uploaded successfully!")
